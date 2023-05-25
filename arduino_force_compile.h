515f4d2c34f2f3959feae0d890da47443f68c1c6
Randall Scharpf <randall.scharpf@gmail.com>
2023-05-24 22:47:42 -0700

    begin the arduino compilation step setup

---
+++ arduino_force_compile.h
@@ -0,0 +1,11 @@
+// hey you
+// include all the project C files in order
+// yes all the C files
+// yes they have to be in a topological-sort-by-dependency order
+// it's cuz Arduino IDE is bad
+// shut up
+// i said shut up
+
+#include "HAL_ATMEGA2560.c"
+#include "checksum.c"
+#include "FlightStateMachine.c"
