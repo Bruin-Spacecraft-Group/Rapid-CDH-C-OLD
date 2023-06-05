29a89e633b8d545fc881e1337166b37055123653
zwang42 <zwang42@g.ucla.edu>
2023-06-05 14:11:44 -0700

    change to checksum function so that it will work with chars, will eventually be changed to use a look-up table

--- checksum.c
+++ checksum.c
@@ -1,7 +1,20 @@
 #include "HAL.h"
 #include "FlightStateMachine.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-unsigned int calculate_checksum(unsigned int x) {
+
+struct char_ptr_with_size {
+    int size;
+    char* data;
+}
+
+char_ptr_with_size calculate_checksum(char[] y, int dataSize) {
+    unsigned int x = 0;
+    for (int i = 0; i<dataSize; i++)
+    {
+        x = x + (y[i] << ((dataSize-i)*8));
+    }
     unsigned int result = 0;
     unsigned int current_index = 3; //start index at 3, count up but only use numbers with Hamming weight >=2
     unsigned int number_to_skip = 4; //rather than take the log of every number (or computing a bitwise and), keep track of which number is next to skip
@@ -24,5 +37,20 @@ unsigned int calculate_checksum(unsigned int x) {
         result++;
     else if (last_2_bits % 4 == 3)
         result--;
-    return result;   
+
+    char_ptr_with_size final_res;
+    final_res.size = 1;
+    if (result >> 24 != 0)
+        final_res.size = 4;
+    else if (result >> 16 != 0)
+        final_res.size  = 3;
+    else if(result >> 8 != 0)
+        final_res.size = 2;
+    
+    final_res.data = (char*)malloc(final_res.size()*sizeof(char));
+    for (int i = 0; i<final_res.size; i++)
+    {
+        final_res.data[i] = result << ((final_res.size-i)*8);
+    }
+    return final_res;   
 }
